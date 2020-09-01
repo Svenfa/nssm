271029fd3d4dffc00537714e25575795f76341b8
SvenFa <github@thenextdomain.de>
2020-09-01 12:42:52 +0200

    Added nssm parameter for appdirectory and logfile

--- manifests/set.pp
+++ manifests/set.pp
@@ -14,10 +14,6 @@
 #   Allow service to interact with desktop
 #   Defaults to false
 #
-# [*nssm_path*]
-#   PATH to nssm exe
-#   defaults to C:\Program Files\nssm-2.24\win64
-#
 define nssm::set (
   $create_user         = false,
   $service_name        = $title,
@@ -25,7 +21,8 @@ define nssm::set (
   $service_pass        = undef,
   $service_interactive = false,
   $app_parameters      = undef,
-  $nssm_path           = 'C:\Program Files\nssm-2.24\win64',
+  $appdirectory        = undef,
+  $logfile             = undef,
 ) {
 
   if $create_user {
@@ -46,23 +43,40 @@ define nssm::set (
   # http://grokbase.com/t/gg/salt-users/152vyb5vx1/weird-whitespace-problem-getting-data-out-of-cmd-run-nssm-on-windows
   exec { 'set_service_name':
     command  => $command,
-    path     => $nssm_path,
-    unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$a = & \"${nssm_path}\\nssm\" get '${service_name}' ObjectName; \$cmp = \$a.Contains(\"${service_user}\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
+    path     => 'C:\Program Files\nssm-2.24\win64',
+    unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$args = nssm get '${service_name}' ObjectName; \$cmp = \$args.Contains(\"${service_user}\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
     provider => powershell,
   }
 
   exec { 'set_app_parameters':
     command  => "nssm set '${service_name}' AppParameters '${app_parameters}'",
-    path     => $nssm_path,
-    unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$a = & \"${nssm_path}\\nssm\" get '${service_name}' AppParameters; \$cmp = \$a.Contains(\"${app_parameters}\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
+    path     => 'C:\Program Files\nssm-2.24\win64',
+    unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$args = nssm get '${service_name}' AppParameters; \$cmp = \$args.Contains(\"${app_parameters}\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
     provider => powershell,
   }
-
+  exec { 'set_appdirectory':
+    command  => "nssm set '${service_name}' AppDirectory '${appdirectory}'",
+    path     => 'C:\Program Files\nssm-2.24\win64',
+    unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$args = nssm get '${service_name}' AppDirectory; \$cmp = \$args.Contains(\"${appdirectory}\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
+    provider => powershell,
+  }
+  exec { 'set_app_stdout':
+    command  => "nssm set '${service_name}' AppStdout '${logfile}'",
+    path     => 'C:\Program Files\nssm-2.24\win64',
+    unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$args = nssm get '${service_name}' AppStdout; \$cmp = \$args.Contains(\"${logfile}\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
+    provider => powershell,
+  }
+  exec { 'set_app_errout':
+    command  => "nssm set '${service_name}' AppStderr '${logfile}'",
+    path     => 'C:\Program Files\nssm-2.24\win64',
+    unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$args = nssm get '${service_name}' AppStderr; \$cmp = \$args.Contains(\"${logfile}\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
+    provider => powershell,
+  }
   if $service_interactive {
     exec { 'set_service_interactive_process':
       command  => "nssm reset '${service_name}' ObjectName; nssm set '${service_name}' Type SERVICE_INTERACTIVE_PROCESS",
-      path     => $nssm_path,
-      unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$a = & \"${nssm_path}\\nssm\" get '${service_name}' Type; \$cmp = \$a.Contains(\"INTERACTIVE\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
+      path     => 'C:\Program Files\nssm-2.24\win64',
+      unless   => "[Console]::OutputEncoding = [System.Text.Encoding]::Unicode; \$args = nssm get '${service_name}' Type; \$cmp = \$args.Contains(\"INTERACTIVE\"); if (\$cmp -eq \"True\") {exit 0} else {exit 1}",
       provider => powershell,
     }
   }
